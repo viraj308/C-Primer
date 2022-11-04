2826bb9a58af1d70e36105b23d695cb95250e454
Viraj Chavan <virajchavan308@gmail.com>
2022-11-04 16:52:24 +0530

    second commit

---
+++ 1.5/text.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+int main() {
+
+	int x, y;
+	std::cout << "Enter two number to multiply: ";
+	std::cin  >> x >> y;
+	std::cout << "Product of ";
+	std::cout << x;
+	std::cout << " and ";
+	std::cout << y;
+	std::cout << " is ";
+	std::cout << x * y;
+	std::cout << "\n";
+}
